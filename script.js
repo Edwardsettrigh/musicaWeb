093d8df9e718ea9b5dcda1eae4164ef43a3a5f85
Edwardsettrigh <eduardorizogonzales9@gmail.com>
2024-07-05 11:21:26 -0600

    Uploadind my fisrt webpage

---
+++ script.js
@@ -0,0 +1,8 @@
+JavaScript
+// Ejemplo de función para reproducir una canción al hacer clic en un botón (no incluido en el HTML)
+function reproducirCancion() {
+    var audio = document.getElementById("audio-player");
+    audio.play();
+}
+
+// Ejemplo de evento para pausar la música al hacer clic en otro botón
